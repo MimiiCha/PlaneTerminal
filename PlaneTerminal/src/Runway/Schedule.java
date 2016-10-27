8accf05aa497f7049629cc0ff2e1c999bb28f7b7
MimiiCha <mimii.cha@live.fr>
2016-10-27 16:05:24 +0200

    Add methods to the Schedule class

--- PlaneTerminal/src/Runway/Schedule.java
+++ PlaneTerminal/src/Runway/Schedule.java
@@ -5,13 +5,24 @@ public class Schedule {
 	private int hour;
 	private int minute;
 	
-	public Schedule() {
-		
+	public Schedule() { //Constructeur par défaut
+		this.hour = 12;
+		this.minute = 0;		
+		//this(12,0);
 	}
     
 	public Schedule(int hour, int minute) { //Constructeur valué
-		this.hour = hour;
-		this.minute = minute;
+		this.hour = hour%24+minute/60;
+		this.minute = minute%60;
 	}
 
+	public String toString() {
+		String s="";
+		if(this.hour<10) s+="0";
+		s+=this.hour;
+		s+=":";
+		if(this.minute<10) s+="0";
+		s+=this.minute;
+		return s;
+	}
 }
