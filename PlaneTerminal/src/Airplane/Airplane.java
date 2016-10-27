a7268033d35f6fc318dece4b2fcda199797f0da6
MimiiCha <mimii.cha@live.fr>
2016-10-27 16:23:30 +0200

    Add methods to the Airplane class

--- PlaneTerminal/src/Airplane/Airplane.java
+++ PlaneTerminal/src/Airplane/Airplane.java
@@ -1,5 +1,23 @@
 package Airplane;
 
 public class Airplane {
+	
+	private boolean landed;
+	
+	public Airplane() {
+		this.landed = true;
+	}
+	
+	public void takeoff() {
+		this.landed = false;
+	}
+	
+	public void landing() {
+		this.landed = true;
+	}
+	
+	public boolean isLanded() {
+		return this.landed;
+	}
 
 }
